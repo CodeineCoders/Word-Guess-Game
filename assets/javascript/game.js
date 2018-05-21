4b71601a7bff363fe498fba078cc9767a1250555
Brian <rac3k@aol.com>
2018-05-21 19:10:30 -0400

    playerGuesses and random compGuesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,17 @@
-var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    console.log(letter);
+alert("Guess the correct letter?")
 
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    console.log(letters);
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 0;
+var guesses = 0;
+
+document.onkeyup = function(event) {
+
+var playerGuesses = (event.key).toLowerCase;
+
+var compGuesses = letters[Math.floor(Math.random() * letters.length)];
+    console.log(compGuesses);
+}
