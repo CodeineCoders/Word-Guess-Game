31a0772625f0667c893bb408c4d1a4fa168df51d
Brian <rac3k@aol.com>
2018-05-25 20:32:34 -0400

    resets applied

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,43 @@
-alert("Guess the correct letter?")
-
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    console.log(letters);
-
-var wins = 0;
-var losses = 0;
-var guessesLeft = 0;
-var guesses = 0;
-
-document.onkeyup = function(event) {
-
-var playerGuesses = event.key.toLowerCase();
+console.log(letters);
+alert("Guess the correct letter?")
+var wins = 1;
+var losses = 1;
+var guessesLeft = 9;
+var GuessArr = [];
 
 var compGuesses = letters[Math.floor(Math.random() * letters.length)]
-    console.log(compGuesses);
+console.log(compGuesses);
 
-if (playerGuesses === compGuesses) {
-    alert('You guessed right!!');
-    console.log(playerGuesses);
-}
+
+document.onkeyup = function (event) {
+
+    var playerGuesses = event.key.toLowerCase();
+
+    var reset = function () {
+        guessesLeft = 9;
+
+    }
+
+    if (playerGuesses == compGuesses) {
+        alert('You guessed right!!');
+        document.getElementById('wins').innerHTML = ('Wins: ' + wins++);
+        alert('Play Again ?');
+        reset();
+        console.log(playerGuesses);
+    }
+
+    else if (playerGuesses != compGuesses) {
+        document.getElementById('guessLeft').innerHTML = ('Guesses Left: ' + guessesLeft--);
+        document.getElementById('guesses').innerHTML = ('Your Guesses so far: ' + playerGuesses);
+
+    }
+
+    if (guessesLeft == 0) {
+        alert('You Lost, Play again? ');
+        document.getElementById('losses').innerHTML = ('Losses: ' + losses++);
+        reset();
+    }
+
 
 }
